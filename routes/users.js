f518ddc3385c839b295b55b475abaf1a8edc1a91
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-16 22:38:13 +0200

    user validation added

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,41 @@
 const express = require('express')
 const router = express.Router()
 
-// Register
+// Register page
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
-// Login
+// Login page
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+// Register User
+router.post('/register', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  const errors = req.validationErrors()
+
+  if(errors){
+    res.render('register', {
+      errors: errors
+    })
+  } else {
+    console.log('Passed')
+  }
 })
 
 module.exports = router
