c51d3735bf706a22d06ac6c94f3800b87939b045
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-16 23:34:44 +0200

    user login working

--- models/users.js
+++ models/users.js
@@ -23,10 +23,29 @@ const UserSchema = mongoose.Schema({
 const User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = (newUser, callback) => {
+	// the function createUser, is going to encrypt the password
+	// that is calles 'hash'
   bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(newUser.password, salt, function(err, hash) {
-        newUser.password = hash;
+				newUser.password = hash;
+				// save is a funcion from mongoose
         newUser.save(callback);
     });
   });
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	let query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const router = express.Router()
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const User = require('../models/users')
 
 // Register page
@@ -54,5 +57,41 @@ router.post('/register', (req, res) => {
     res.redirect('/users/login')
   }
 })
+
+passport.use(new LocalStrategy(
+	function (username, password, done) {
+		User.getUserByUsername(username, function (err, user) {
+			if (err) throw err;
+			if (!user) {
+				return done(null, false, { message: 'Unknown User' });
+			}
+
+			User.comparePassword(password, user.password, function (err, isMatch) {
+				if (err) throw err;
+				if (isMatch) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: 'Invalid password' });
+				}
+		  });
+	  });
+}));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.getUserById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+	passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+	function (req, res) {
+		res.redirect('/');
+	});
+
 
 module.exports = router
